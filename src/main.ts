b6ddb35ccb191f423d88fc0988f2bbf827e9081d
Rigel Lago <rigel.lago@gaatu.com>
2024-10-30 21:04:05 +0800

    update cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,21 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     // Allow socket.io to use CORS
   app.useWebSocketAdapter(new IoAdapter(app));
-  app.enableCors(corsConfig); 
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'https://http-monitor-client.vercel.app',
+        'https://http-monitor-client-6kioj8j41-rigels-projects-85d99a61.vercel.app'
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
