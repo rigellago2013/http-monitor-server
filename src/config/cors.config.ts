5a173b48e9d89edc9df012a5ba08d8fe05f455ae
Rigel Lago <rigel.lago@gaatu.com>
2024-10-30 20:45:05 +0800

    update cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,5 +1,3 @@
-import { CLIENT_ORIGIN } from '../common/utils/constants';
-
 export const corsConfig = {
   origin: '*',
   methods: ['GET', 'POST']
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { corsConfig } from './config/cors.config';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    // Allow socket.io to use CORS
+  app.useWebSocketAdapter(new IoAdapter(app));
   app.enableCors(corsConfig); 
   await app.listen(3000);
 }
