2b2c5ede1ee602f048777cd21fec4881d14a0b8d
Rigel Lago <rigel.lago@gaatu.com>
2024-10-30 22:11:38 +0800

    update

--- src/common/utils/constants.ts
+++ src/common/utils/constants.ts
@@ -1,2 +1,2 @@
 export const CLIENT_ORIGIN = 'https://http-monitor-client.vercel.app';
-export const CLIENT_SOCKET_ORIGIN = 'https://http-monitor-client-6kioj8j41-rigels-projects-85d99a61.vercel.app';
+export const CLIENT_SOCKET_ORIGIN = 'https://http-monitor-client.vercel.app:80';
--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,4 +1,4 @@
-import { CLIENT_ORIGIN } from "../../src/common/utils/constants";
+import { CLIENT_ORIGIN } from "../common/utils/constants";
 
 export const corsConfig = {
   origin: CLIENT_ORIGIN,
