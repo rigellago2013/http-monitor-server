a75562435cfed217b30d9cb028ec3fa957756372
Rigel Lago <rigel.lago@gaatu.com>
2024-10-30 22:27:55 +0800

    update

--- src/common/utils/constants.ts
+++ src/common/utils/constants.ts
@@ -1,2 +1,2 @@
 export const CLIENT_ORIGIN = 'https://http-monitor-client.vercel.app';
-export const CLIENT_SOCKET_ORIGIN = 'https://http-monitor-client.vercel.app:80';
+export const CLIENT_SOCKET_ORIGIN = 'ws://http-monitor-client.vercel.app:80';
