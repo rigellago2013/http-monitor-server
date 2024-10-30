040f8996d783efa065b851250e2091418de44dd2
Rigel Lago <rigel.lago@gaatu.com>
2024-10-31 02:06:59 +0800

    update ping module

--- src/modules/ping/ping.module.ts
+++ src/modules/ping/ping.module.ts
@@ -4,13 +4,12 @@ import { Response, ResponseSchema } from 'src/schemas/response.schema';
 import { PingDAO } from './dao/ping.dao';
 import { PingController } from './ping.controller';
 import { PingService } from './ping.service';
-import { PingGateway } from './ping.gateway';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Response.name, schema: ResponseSchema }])
   ],
-  providers: [PingDAO, PingService, PingGateway],
+  providers: [PingDAO, PingService],
   controllers: [PingController]
 })
 export class PingModule {}
